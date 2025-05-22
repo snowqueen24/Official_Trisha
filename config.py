5e47999efb583a47e6844145bd91e5343a1a0b1f
eyeyadav31 <eyeyadav31@gmail.com>
2025-05-22 19:08:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/fighterrbd/Jani_RP_Music",
+    "https://github.com/eyeyadav31/Jani_RP_Music1234",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(

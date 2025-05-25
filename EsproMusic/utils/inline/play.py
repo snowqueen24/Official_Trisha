0265a57ef80d9ba9258ef6e1f85404cb24549f21
eyeyadav31 <eyeyadav31@gmail.com>
2025-05-25 16:14:38 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -60,11 +60,11 @@ def stream_markup_timer(_, chat_id, played, dur):
           #  InlineKeyboardButton(text="➘", callback_data=f"ADMIN Stop|{chat_id}"),
             #InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-       # [
-            #InlineKeyboardButton(
-               # text=f"{played} {bar} {dur}",
-                #callback_data="GetTimer",
-            #)
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]

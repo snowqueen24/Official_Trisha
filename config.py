3645c3679ac5d9627ff29d7ca2f3c3effb5fce8d
ankitsingh12134 <ms0258756@gmail.com>
2025-06-26 05:33:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+pYRhze6SVqgyMWQ1")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+pYRhze6SVqgyMWQ1")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+eDGuiLMGvO0zNmFl")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+eDGuiLMGvO0zNmFl")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
 API_KEY = getenv("API_KEY", '30DxNexGenBots667d40')
@@ -81,21 +81,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/74y7ob.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/l7n2co.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/74y7ob.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/l7n2co.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/yd51bx.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/yd51bx.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/yd51bx.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/l7n2co.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/l7n2co.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/l7n2co.jpg"
 
 
 def time_to_seconds(time):
